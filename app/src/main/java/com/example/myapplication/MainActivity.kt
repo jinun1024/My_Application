f0b8ecab43583162e08aff8e555979e6f84885b1
jinun1024 <bill1024@naver.com>
2023-07-12 14:00:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// 일단 엑티비티 화면 꾸밀겡 ㅕ
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
